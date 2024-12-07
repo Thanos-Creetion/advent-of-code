2a53ba4eaf54841b3af13467aafa1b679f4a0324
Thanos-Creetion <138450489+Thanos-Creetion@users.noreply.github.com>
2024-12-07 21:20:15 +0100

    More day 7

--- 2024/Day 7/index.mjs
+++ 2024/Day 7/index.mjs
@@ -24,12 +24,29 @@ let permutations = [];
 
 for (let i = 0; i < lines.length; i++) {
     let line = lines[i];
-    
     permutations.push(permutator(line[1].length - 1, 2));
 }
 
+// Check if equations are possible for each line
+for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    let permutation = permutations[i];
+    let lineSum = 0;
 
-// Check if equations are possible
+    console.log(line);
+    console.log(permutation);
+
+    // Go through each combination of permutations
+    for (let j = 0; j < permutation.length; j++) {
+
+    }
+
+    // Go through this permutation
+
+    for (let j = 1; j < line[1].length; j++) {
+        // lineSum += expression(line[1][j - 1], line[1][j], permutation[]);
+    }
+}
 
 
 
@@ -55,51 +72,89 @@ function expression(x, y, operator) {
 function permutator(length, numbers) {
     // Initialize variables
     let list = [];
-    let max = Math.pow(length, numbers);
-
-    // Set up the base permutation and push it to the list
     let base = [];
+    let max = 0;
 
+    // Check what the max number is
+    if (length == 1) {
+        max = 2;
+    }
+    else {
+        max = Math.pow(numbers, length);
+    }
+
+    // Set up the base array and copy it to the list
     for (let i = 0; i < length; i++) {
         base.push(1);
     }
 
+    // Copy the base to the list
     list.push(base);
 
     // Now create all the other permutations
-    for (let i = 0; i < max; i++) {
-        let permutation = base;
-        let index = length - 1;
-
-        if (permutation[index] < numbers) {
-            permutation[index]++;
-            list.push(permutation);
-        }
-        else if (index != 0) {
-            index--;
-        }
-    }
+    permute(list, base, numbers, max);
 
     return list
 }
 
 // Get one permutation
-function permute(list, array, index, numbers) {
-    if (array[index] < numbers) {
-        array[index]++;
-        list.push(array);
-    }
-    else if (index != 0) {
-        index--;
-
-
-
+function permute(list, previous, numbers, max) {
+    if (list.length == max) {
+        return;
     }
 
-    for (let i = 0; i < array.length; i++) {
-        
-        
+    // Deep copy the array
+    let current = previous.slice();
+
+    // Create the indexes
+    let index = -1;
+    let lastIndex = -1;
+
+    // Find the index of the number to increase
+    for (let i = 0; i < current.length; i++) {
+        if (current[current.length - 1 - i] < numbers && index == -1) {
+            index = current.length - 1 - i;
+            lastIndex = index + 1;
+        }
     }
 
+    // Create another array and deep copy the current
+    let next = [];
 
+    // Check if the last index exists
+    if (!(current[lastIndex] === undefined)) {
+        // Now if the new number is equal to number set all the following numbers to one
+        if (current[lastIndex] == numbers) {
+            next = current.slice();
+            next[index]++;
+
+            for (let i = lastIndex; i < current.length; i++) {
+                next[i] = 1;
+            }
+
+            // Set index to -1 so the functions skips to the next recursion
+            index = -1;
+
+            // Now push the next array to the list
+            list.push(next);
+        }
+    }
+    
+    // Push the array to the list
+    if (list.length == max) {
+        return;
+    }
+
+    if (index != -1) {
+        current[index]++;
+        list.push(current);
+    }
+
+    // Check which array needs to used for recursion
+    if ((next[0] === undefined)) {
+        permute(list, current, numbers, max);
+    }
+    else {
+        permute(list, next, numbers, max);
+    }
 }
--- 2024/Day 7/test.mjs
+++ 2024/Day 7/test.mjs
@@ -1,21 +1,95 @@
-const permutator = (inputArr) => {
-    let result = [];
-  
-    const permute = (arr, m = []) => {
-      if (arr.length === 0) {
-        result.push(m)
-      } else {
-        for (let i = 0; i < arr.length; i++) {
-          let curr = arr.slice();
-          let next = curr.splice(i, 1);
-          permute(curr.slice(), m.concat(next))
-       }
-     }
-   }
-  
-   permute(inputArr)
-  
-   return result;
+// To log the test function
+let permutations = permutator(4, 2).slice();
+console.log(permutations);
+
+
+// Get all possible permutations
+function permutator(length, numbers) {
+    // Initialize variables
+    let list = [];
+    let base = [];
+    let max = 0;
+
+    // Check what the max number is
+    if (length == 1) {
+        max = 2;
+    }
+    else {
+        max = Math.pow(numbers, length);
+    }
+
+    // Set up the base array and copy it to the list
+    for (let i = 0; i < length; i++) {
+        base.push(1);
+    }
+
+    // Copy the base to the list
+    list.push(base);
+
+    // Now create all the other permutations
+    permute(list, base, numbers, max);
+
+    return list
 }
 
-console.log(permutator([1,2,3]));
+// Get one permutation
+function permute(list, previous, numbers, max) {
+    if (list.length == max) {
+        return;
+    }
+
+    // Deep copy the array
+    let current = previous.slice();
+
+    // Create the indexes
+    let index = -1;
+    let lastIndex = -1;
+
+    // Find the index of the number to increase
+    for (let i = 0; i < current.length; i++) {
+        if (current[current.length - 1 - i] < numbers && index == -1) {
+            index = current.length - 1 - i;
+            lastIndex = index + 1;
+        }
+    }
+
+    // Create another array and deep copy the current
+    let next = [];
+
+    // Check if the last index exists
+    if (!(current[lastIndex] === undefined)) {
+        // Now if the new number is equal to number set all the following numbers to one
+        if (current[lastIndex] == numbers) {
+            next = current.slice();
+            next[index]++;
+
+            for (let i = lastIndex; i < current.length; i++) {
+                next[i] = 1;
+            }
+
+            // Set index to -1 so the functions skips to the next recursion
+            index = -1;
+
+            // Now push the next array to the list
+            list.push(next);
+        }
+    }
+    
+    // Push the array to the list
+    if (list.length == max) {
+        return;
+    }
+
+    if (index != -1) {
+        current[index]++;
+        list.push(current);
+    }
+
+    // Check which array needs to used for recursion
+    if ((next[0] === undefined)) {
+        permute(list, current, numbers, max);
+    }
+    else {
+        permute(list, next, numbers, max);
+    }
+}
